fcd86fee0f4903886293b5dd03809c3d72b5dab8
pinguo-marui <marui@camera360.com>
2014-10-09 20:57:16 +0800

    笔试题测试代码修改

--- app/src/main/java/com/okry/amt/temp/MyActivity.java
+++ app/src/main/java/com/okry/amt/temp/MyActivity.java
@@ -3,59 +3,97 @@ package com.okry.amt.temp;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.okry.amt.R;
 import com.okry.amt.allbase.BaseActivity;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MyActivity extends BaseActivity {
 
-    ViewGroup mContainer;
-    ImageView[] mImageViews;
+    String[] imageFilePaths = {"/storage/emulated/0/test/img0.png", "/storage/emulated/0/test/img1.png", "/storage/emulated/0/test/img2.png",
+            "/storage/emulated/0/test/img3.png", "/storage/emulated/0/test/img4.png", "/storage/emulated/0/test/img5.png",
+            "/storage/emulated/0/test/img6.png", "/storage/emulated/0/test/img7.png", "/storage/emulated/0/test/img8.png", "/storage/emulated/0/test/img9.png"};
+    private ListView mListView;
 
-    int[] imgRes = {R.drawable.img0, R.drawable.img1, R.drawable.img2,
-            R.drawable.img3, R.drawable.img4, R.drawable.img5,
-            R.drawable.img6, R.drawable.img7, R.drawable.img8,
-            R.drawable.img9};
-    private Handler myHandler = new Handler();
+    private static Bitmap getBitmap(String filePath) {
+        Bitmap b = BitmapFactory.decodeFile(filePath);
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return b;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_img_container);
-        mContainer = (ViewGroup) findViewById(R.id.img_container);
-        mImageViews = new ImageView[mContainer.getChildCount()];
-        for (int i = 0; i < mImageViews.length; i++) {
-            mImageViews[i] = (ImageView) mContainer.getChildAt(i);
-        }
-//        loadImage();
-        loadImage2();
+        mListView = (ListView) findViewById(R.id.list);
+        MyAdapter adapter = new MyAdapter();
+        adapter.setData(Arrays.asList(imageFilePaths));
+        mListView.setAdapter(adapter);
     }
 
-    private void loadImage() {
-        for (int i = 0; i < mImageViews.length; i++) {
-            mImageViews[i].setImageResource(imgRes[i]);
-        }
-    }
+    // 注意MyAdapter是static类，不能使用Activity的方法
+    private static class MyAdapter extends BaseAdapter {
 
-    private void loadImage2() {
-        new Thread() {
-            @Override
-            public void run() {
-                for (int i = 0; i < mContainer.getChildCount(); i++) {
-                    final int index = i;
-                    final Bitmap b = BitmapFactory.decodeResource(getResources(), imgRes[i]);
-                    myHandler.post(new Runnable() {
+        private List<String> mList;
+
+        public void setData(List<String> list) {
+            mList = list;
+        }
+
+        @Override
+        public int getCount() {
+            return mList == null ? 0 : mList.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return mList == null ? null : mList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            // convertView的重用是考察点
+            if (convertView == null) {
+                ImageView imageView = new ImageView(parent.getContext());
+                convertView = imageView;
+            }
+            final ImageView imageView = (ImageView) convertView;
+
+            // 直接在主线程加载会卡顿
+            //            Bitmap b = getBitmap(getItem(position));
+            //            imageView.setImageBitmap(b);
+            // 这里的异步实现写得比较烂，只是完成了功能，方法仅供参考。。。。。。
+            new Thread() {
+                @Override
+                public void run() {
+                    final Bitmap b = getBitmap(getItem(position));
+                    imageView.post(new Runnable() {
                         @Override
                         public void run() {
-                            mImageViews[index].setImageBitmap(b);
+                            imageView.setImageBitmap(b);
                         }
                     });
                 }
-            }
-        }.start();
+            }.start();
+            return convertView;
+        }
+
     }
 
 }
